16310c40199b3e39508ca4e864255b3d4ed963a1
Félicien Dirand <feliciendirand@MacBook-Pro-de-Felicien.local>
2025-09-09 14:47:34 +0200

    Specialite et Practicien entitee

--- app/src/application_core/domain/entities/praticien/Praticien.php
+++ app/src/application_core/domain/entities/praticien/Praticien.php
@@ -5,6 +5,45 @@ namespace toubilib\core\domain\entities\praticien;
 
 class Praticien
 {
- 
+    private int $id;
+    private string $nom;
+    private string $prenom;
+    private string $ville;
+    private string $email;
+    private Specialite $specialite;
 
+
+    public function __construct(int $id, string $nom, string $prenom, string $ville, string $email, Specialite $specialite)
+    {
+        $this->id = $id;
+        $this->nom = $nom;
+        $this->prenom = $prenom;
+        $this->ville = $ville;
+        $this->email = $email;
+        $this->specialite = $specialite;
+    }
+    public function getId(): int
+    {
+        return $this->id;
+    }
+    public function getNom(): string
+    {
+        return $this->nom;
+    }
+    public function getPrenom(): string
+    {
+        return $this->prenom;
+    }
+    public function getVille(): string
+    {
+        return $this->ville;    
+    }
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+    public function getSpecialite(): Specialite
+    {
+        return $this->specialite;
+    }
 }
